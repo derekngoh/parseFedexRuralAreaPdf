4358384e041c8ca48d6fb7f030801c69c1d2f643
derek <derek@justship.sg>
2021-01-10 10:51:49 +0800

    fixed setUp() not initialising issue

--- parseRuralList_Test.py
+++ parseRuralList_Test.py
@@ -9,9 +9,9 @@ class RuralListTestCase(unittest.TestCase):
 
     def setUp(self): 
         try:
-            testPrlApp = prl()
-            ruralPdfList = testPrlApp.ruralOriginalPDFFileAbsPath
-            testPrlApp.createCsvRawRuralList(ruralPdfList)
+            self.testPrlApp = prl()
+            self.ruralPdfList = self.testPrlApp.ruralOriginalPDFFileAbsPath
+            self.testPrlApp.createCsvRawRuralList(ruralPdfList)
         except FileNotFoundError:
             print("Please check if PDF file exists before re-running the application.")
         except NameError:
@@ -19,69 +19,62 @@ class RuralListTestCase(unittest.TestCase):
 
     def tearDown(self):
         try:
-            os.remove(psl.getRuralRawCsvAbsPath())
+            os.remove(self.prl().getRuralRawCsvAbsPath())
         except FileNotFoundError:
             print("File may have already been deleted.")
         except Exception:
             print(sys.exc_info()[0])
 
     def test_rawCsvFileExists(self):
-        testPrlApp = prl()
-        self.assertTrue(os.path.isfile(testPrlApp.getRuralRawCsvAbsPath()))
+        self.assertTrue(os.path.isfile(self.testPrlApp.getRuralRawCsvAbsPath()))
 
     def test_newCombinedCountryNameFileCreation(self):
-        testPrlApp = prl()
-        rawCsvRuralList = testPrlApp.getRuralRawCsvAbsPath()
-        testPrlApp.createNewFileWithCombinedCountryName(rawCsvRuralList)
-        self.assertTrue(os.path.isfile(testPrlApp.getCsvCombinedCountryNamesAbsPath()))
+        rawCsvRuralList = self.testPrlApp.getRuralRawCsvAbsPath()
+        self.testPrlApp.createNewFileWithCombinedCountryName(rawCsvRuralList)
+        self.assertTrue(os.path.isfile(self.testPrlApp.getCsvCombinedCountryNamesAbsPath()))
 
     def test_allCountriesAccountedFor(self):
-        testPrlApp = prl()
-        csvWithCombinedNames = testPrlApp.getCsvCombinedCountryNamesAbsPath()
-        self.assertTrue(testPrlApp.accountForAllCountries(csvWithCombinedNames))
+        csvWithCombinedNames = self.testPrlApp.getCsvCombinedCountryNamesAbsPath()
+        self.assertTrue(self.testPrlApp.accountForAllCountries(csvWithCombinedNames))
 
     def test_comparePagesOfOriginalPdfAndCsv(self):
-        testPrlApp = prl()
-        numOfPdfPages = testPrlApp.ruralOriginalPDFNumOfPage
-        csvWithCombinedNames = testPrlApp.getCsvCombinedCountryNamesAbsPath()
-        keyValuePairsOfAllCells = testPrlApp.getKeyValuePairsNoPageNumForCells(csvWithCombinedNames)
-        numOfCsvPages = len(testPrlApp.getRowBreakIndex(keyValuePairsOfAllCells))
+        numOfPdfPages = self.testPrlApp.ruralOriginalPDFNumOfPage
+        csvWithCombinedNames = self.testPrlApp.getCsvCombinedCountryNamesAbsPath()
+        keyValuePairsOfAllCells = self.testPrlApp.getKeyValuePairsNoPageNumForCells(csvWithCombinedNames)
+        numOfCsvPages = len(self.testPrlApp.getRowBreakIndex(keyValuePairsOfAllCells))
         self.assertEqual(numOfPdfPages, numOfCsvPages)
 
     def test_compareKeyValueCountWithAndWithoutPage(self):
-        testPrlApp = prl()
-        csvWithCombinedNames = testPrlApp.getCsvCombinedCountryNamesAbsPath()
-        keyValuePairsOfAllCells = testPrlApp.getKeyValuePairsNoPageNumForCells(csvWithCombinedNames)
+        csvWithCombinedNames = self.testPrlApp.getCsvCombinedCountryNamesAbsPath()
+        keyValuePairsOfAllCells = self.testPrlApp.getKeyValuePairsNoPageNumForCells(csvWithCombinedNames)
         lenOfKeyValuePairs = len(keyValuePairsOfAllCells)
-        rowBreakIndex = testPrlApp.getRowBreakIndex(keyValuePairsOfAllCells)
-        keyValueWithPageCount = testPrlApp.getKeyValuePairsWithPageForCells(keyValuePairsOfAllCells, rowBreakIndex, csvWithCombinedNames)
+        rowBreakIndex = self.testPrlApp.getRowBreakIndex(keyValuePairsOfAllCells)
+        keyValueWithPageCount = self.testPrlApp.getKeyValuePairsWithPageForCells(keyValuePairsOfAllCells, rowBreakIndex, csvWithCombinedNames)
         lenOfKeyValueWithPageCount = len(keyValueWithPageCount)
         self.assertEqual(lenOfKeyValuePairs, lenOfKeyValueWithPageCount)
 
     def test_compareRefListCountryCountAndCountryDictCount(self):
-        testPrlApp = prl()
-        csvWithCombinedNames = testPrlApp.getCsvCombinedCountryNamesAbsPath()
-        keyValuePairsOfAllCells = testPrlApp.getKeyValuePairsNoPageNumForCells(csvWithCombinedNames)
-        rowBreakIndex = testPrlApp.getRowBreakIndex(keyValuePairsOfAllCells)
-        keyValueWithPages = testPrlApp.getKeyValuePairsWithPageForCells(keyValuePairsOfAllCells, rowBreakIndex, csvWithCombinedNames)
-        countryRawDictCount = len(testPrlApp.countryPostalCodeRawDict(keyValueWithPages, rowBreakIndex))
-        refListCount = len(testPrlApp.getCountriesInRuralList())-1
+        csvWithCombinedNames = self.testPrlApp.getCsvCombinedCountryNamesAbsPath()
+        keyValuePairsOfAllCells = self.testPrlApp.getKeyValuePairsNoPageNumForCells(csvWithCombinedNames)
+        rowBreakIndex = self.testPrlApp.getRowBreakIndex(keyValuePairsOfAllCells)
+        keyValueWithPages = self.testPrlApp.getKeyValuePairsWithPageForCells(keyValuePairsOfAllCells, rowBreakIndex, csvWithCombinedNames)
+        countryRawDictCount = len(self.testPrlApp.countryPostalCodeRawDict(keyValueWithPages, rowBreakIndex))
+        refListCount = len(self.testPrlApp.getCountriesInRuralList())-1
         self.assertEqual(refListCount, countryRawDictCount)
 
     def test_checkNumRangeValues(self):
-        testPrlApp = prl()
         sampleRange = "35000-35019"
-        sampleParsedRange = [35000, 35001, 35002, 35003, 35004, 35005, 35006, 35007, 35008, 35009, 35010, 35011, 35012, 35013, 35014, 35015, 35016, 35017, 35018, 35019]
-        parseResult = testPrlApp.detectAndParseRange(sampleRange)
+        sampleParsedRange = ["35000", "35001", "35002", "35003", "35004", "35005", "35006", "35007", "35008", "35009", "35010", "35011", "35012", "35013", "35014", "35015", "35016", "35017", "35018", "35019"]
+        parseResult = self.testPrlApp.detectAndParseRange(sampleRange)
         self.assertEqual(parseResult, sampleParsedRange)
 
     # def test_removeNonApplicableDoubleAsterix(self):
-    #     testPrlApp = prl()
-    #     csvWithCombinedNames = testPrlApp.getCsvCombinedCountryNamesAbsPath()
-    #     keyValuePairsOfAllCells = testPrlApp.getKeyValuePairsNoPageNumForCells(csvWithCombinedNames)
-    #     rowBreakIndex = testPrlApp.getRowBreakIndex(keyValuePairsOfAllCells)
-    #     keyValueWithPages = testPrlApp.getKeyValuePairsWithPageForCells(keyValuePairsOfAllCells, rowBreakIndex, csvWithCombinedNames)
-    #     countryRawDict = testPrlApp.countryPostalCodeRawDict(keyValueWithPages, rowBreakIndex)
+    #     self.testPrlApp = prl()
+    #     csvWithCombinedNames = self.testPrlApp.getCsvCombinedCountryNamesAbsPath()
+    #     keyValuePairsOfAllCells = self.testPrlApp.getKeyValuePairsNoPageNumForCells(csvWithCombinedNames)
+    #     rowBreakIndex = self.testPrlApp.getRowBreakIndex(keyValuePairsOfAllCells)
+    #     keyValueWithPages = self.testPrlApp.getKeyValuePairsWithPageForCells(keyValuePairsOfAllCells, rowBreakIndex, csvWithCombinedNames)
+    #     countryRawDict = self.testPrlApp.countryPostalCodeRawDict(keyValueWithPages, rowBreakIndex)
 
 
 if __name__ == "__main__":
